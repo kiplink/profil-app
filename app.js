032efd46ea03ed5a0e781b25b9fc1fbaf30fe3d4
Arif Fajar Irawan <kiplink@.unej.ac.id>
2022-05-21 09:07:46 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+'use strict'
 const http = require('http')
 const express = require('express')
-const path = require('path')
 const app = express()
-app.use(express.json())
-app.use(express.static('express')) // default URL for website
-app.use('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/express/index.html'))
+
+app.use(express.static('public')) // default URL for website
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/express/index.html')
   //__dirname : It will resolve to your project folder.
 })
-const server = http.createServer(app)
+
 const port = 8000
 const host = '0.0.0.0'
-server.listen(port, host)
+app.listen(port, host)
 console.debug('Server listening on port ' + port)
