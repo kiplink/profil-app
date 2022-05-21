b6c55a5ea3f9c85a3f34ab7183235419f440c551
Arif Fajar Irawan <kiplink@.unej.ac.id>
2022-05-21 09:41:58 +0700

    replace all

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello world!\n')
+})
+app.get('/profil', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html')
+})
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
